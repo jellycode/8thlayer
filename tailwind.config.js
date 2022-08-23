bf7a0ed8a350a6edc73a560bff9ec07d72323277
Blair Habicht <blairh@jellycode.com>
2022-08-23 07:34:36 -0600

    Tailwind addition

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: [
+  './assets/**/*.{css,js}',
+  './layout/**/*.liquid',
+  './sections/**/*.{html,js,liquid}',
+  './snippets/**/*.{liquid}',
+  './templates/**/*.{json,liquid}',
+ ],
+ theme: {
+  extend: {},
+ },
+ plugins: [],
+};
